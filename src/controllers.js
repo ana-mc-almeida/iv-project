9e7507d03bf28a6fc3462e8d1829ce144fd18e50
Liliana Sun <liliana2003sun@gmail.com>
2024-10-07 17:10:43 +0100

    feat: filter the controller dataset

--- src/controllers.js
+++ src/controllers.js
@@ -1,1 +1,113 @@
-// TODO
+const filters = {
+    MAP_TYPE: 'none',
+    DISTRICT: [],
+    TYPE: [],
+    CONDITION: [],
+    YEARS: 0
+};
+
+function updateAllCharts(data){
+    // TODO: go to idioms update
+}
+
+function updateMapToArea(){
+    filters.MAP_TYPE = 'area';
+    console.log(filters.MAP_TYPE);
+}
+
+function updateMapToPricePerSquareMeter(){
+    filters.MAP_TYPE = 'price_per_square_meter';
+    console.log(filters.MAP_TYPE);
+}
+
+function updateMapToNumberOfAvailability(){
+    filters.MAP_TYPE = 'number_of_availability';
+    console.log(filters.MAP_TYPE);
+}   
+
+function updateDistrict(data, district){
+
+    const exists = filters.DISTRICT.includes(district);
+    if (!exists) {
+        filters.DISTRICT.push(district); 
+    } else {
+        // remove if exists
+        filters.DISTRICT = filters.DISTRICT.filter(d => d !== district);
+    }
+
+    const filteredData = filterDatasetByDistricts(data); 
+
+    updateAllCharts(filteredData);
+    console.log("distritos:" + filters.DISTRICT);
+    console.log(filteredData);
+}
+
+function updateType(data, type){
+
+    if (!filters.TYPE.includes(type)) {
+        filters.TYPE.push(type); 
+    } else {
+        // remove if exists
+        filters.TYPE = filters.TYPE.filter(d => d !== type);
+    }
+
+    const filteredData = filterDatasetByType(data); 
+
+    updateAllCharts(filteredData);
+    console.log(filters.TYPE);
+    console.log(filteredData);
+}
+
+function updateCondition(data, condition){
+
+    let others = 0;
+    if (condition === 'Others') {
+        others = 1;
+    }
+
+    if (!filters.CONDITION.includes(condition)) {
+        filters.CONDITION.push(condition); 
+    } else {
+        // remove if exists
+        filters.CONDITION = filters.CONDITION.filter(d => d !== condition);
+    }
+
+    console.log("others: " + others);
+
+    const filteredData = filterDatasetByConditions(data, others); 
+
+    updateAllCharts(filteredData);
+    console.log(filters.CONDITION);
+    console.log(filteredData);
+}
+
+function updateYear(years){
+    filters.YEARS = years;
+    console.log(filters.YEARS);
+}
+
+function filterDatasetByDistricts(data) {
+    if (filters.DISTRICT.length === 0) {
+        return data; 
+    }
+    return data.filter(d => filters.DISTRICT.includes(d.District));
+}
+
+function filterDatasetByType(data) {
+    if (filters.TYPE.length === 0) {
+        return data; 
+    }
+    return data.filter(d => filters.TYPE.includes(d.AdsType));
+}
+
+function filterDatasetByConditions(data, others) {
+    if (filters.CONDITION.length === 0) {
+        return data; 
+    }
+
+    if (others){
+        return data.filter(d => filters.CONDITION.includes(d.Condition) || 
+            (d.Condition !== 'New' && d.Condition !== 'Renovated' && d.Condition !== 'Used'));
+    }
+    return data.filter(d => filters.CONDITION.includes(d.Condition));
+}
--- src/script.js
+++ src/script.js
@@ -1,6 +1,22 @@
+var globalData;
+let selectedYears = 0;
+
 function init() {
-  d3.csv("./data/final_dataset.csv").then(function (data) {
-    // TODO
+  d3.json("./data/final_dataset.json", function (d) {
+    // Converte os valores do CSV em números
+    return {
+      Area: +d["Area"],
+      Rooms: +d["Rooms"],
+      Bathrooms: +d["Bathrooms"],
+      // if AdsType is 'Rent' then Price is * 40
+      Price: d["AdsType"] === "Rent" ? +d["Price"] * 50 : +d["Price"],
+      District: d["District"],
+      AdsType: d["AdsType"],
+      Condition: d["Condition"],
+    };
+  }).then(function (data) {
+    globalData = data;
+    console.log(globalData);
   });
 }
 
@@ -37,4 +53,46 @@ document.addEventListener("DOMContentLoaded", function() {
       d3.select(this).classed('selected', !d3.select(this).classed('selected'));
   });
 
+  d3.select('#yearInput').on('input', function() {
+    selectedYears = +this.value;
+    console.log(selectedYears);
+    updateYear(selectedYears); 
+  });
+
 });
+
+function selectMap(typeOfMap){
+  d3.selectAll("button").attr("disabled", true);
+
+  switch (typeOfMap) {
+      case "Area":
+          updateMapToArea();
+          break;
+
+      case "Price Per Square Meter":
+          updateMapToPricePerSquareMeter();
+          break;
+
+      case "Number of availability":
+          updateMapToNumberOfAvailability();
+          break;
+
+      default:
+          break;
+  }
+}
+
+function selectDistrict(district){
+  const data = globalData.slice();
+  updateDistrict(data, district);
+}
+
+function selectType(type){
+  const data = globalData.slice();
+  updateType(data, type);
+}
+
+function selectCondition(condition){
+  const data = globalData.slice();
+  updateCondition(data, condition);
+}
