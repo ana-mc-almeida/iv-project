40a34a0ca6c0eb66b4c13f10c4cb3806636c11e2
Liliana Sun <liliana2003sun@gmail.com>
2024-10-07 05:09:49 +0100

    feat: controller color interaction

--- src/script.js
+++ src/script.js
@@ -3,3 +3,38 @@ function init() {
     // TODO
   });
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  const dropdownLinks = d3.selectAll('.dropdown-content a');
+
+  dropdownLinks.on('click', function(event) {
+      event.preventDefault(); 
+
+      d3.select(this).classed('selected', !d3.select(this).classed('selected'));
+  });
+
+  const dropdownConditionLinks = d3.selectAll('.dropdown-condition-content a');
+
+  dropdownConditionLinks.on('click', function(event) {
+      event.preventDefault();
+
+      d3.select(this).classed('selected', !d3.select(this).classed('selected'));
+  });
+
+  const searchLinks = d3.selectAll('.search-content a');
+
+  searchLinks.on('click', function(event) {
+      event.preventDefault();
+
+      searchLinks.classed('selected', false);
+
+      d3.select(this).classed('selected', true);
+  });
+
+  const buttons = d3.selectAll('.button-type');
+
+  buttons.on('click', function() {
+      d3.select(this).classed('selected', !d3.select(this).classed('selected'));
+  });
+
+});
