5ff6727f22f0d34d5d2da7e18727296576b65af4
Liliana Sun <liliana2003sun@gmail.com>
2024-10-07 03:50:33 +0100

    fix: dropdown condition

--- src/controllers.js
+++ src/controllers.js
@@ -1,11 +1,1 @@
-function selectDistrict(district) {
-}
-
-function selectMap(map) {
-}
-
-function selectCondition(condition) {
-}
-
-function selectType(type) {
-}
+// TODO
--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function init() {
-    d3.csv("./data/final_dataset.csv").then(function (data) {
-      // TODO
-    });
-  }
+  d3.csv("./data/final_dataset.csv").then(function (data) {
+    // TODO
+  });
+}
